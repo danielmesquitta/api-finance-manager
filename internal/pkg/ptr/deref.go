b7092022e592e42bfee6b632891d6ca885ea6097
Daniel Mesquita <danielmesquitta123@gmail.com>
2025-03-27 08:48:08 -0300

    feat: add Coalesce function for safer pointer handling

--- internal/pkg/ptr/deref.go
+++ internal/pkg/ptr/deref.go
@@ -1,14 +1,5 @@
 package ptr
 
-// Coalesce returns *p if p is not nil, otherwise v.
-func Coalesce[T any](p *T, v T) T {
-	if p != nil {
-		return *p
-	}
-
-	return v
-}
-
 // Deref returns *p if p is not nil, otherwise the zero value of T.
 func Deref[T any](p *T) T {
 	return Coalesce(p, *new(T))
